d6faef8e9a4b2be129ca43cf065d26e91896d71a
Munsamy <Prenolan.munsamy@kantar.com>
2021-05-05 09:00:00 +0200

    added validation added basic imputation

--- R/check_flatline.R
+++ R/check_flatline.R
@@ -1,12 +1,21 @@
-#' gets the number of responses that are the same per person
-#' @export
-#' @param df should be a dataframe of numeric variables
+#' check_flatine returns the maximum number of times the most common value occurs per row
+#' missing values can be replaced with one of the mean,mode,min,max per variable
 
-check_flatline <- function(df){
+#' @export
+#' @param df must be a data.frame of numeric variables
+#' @param vars must be a string of variable names to operate on
+#' @param impute_type must be a string of one of "mean","min","max"
+
+check_flatline <- function(df,vars,impute_type="mean"){
   
   # ensuring df is provided
   if (missing(df)){
     stop("df is compulsory")
+  }
+  
+  # ensuring vars is provided
+  if (missing(vars)){
+    stop("vars is compulsory")
   }
   
   # df must be a data.frame or tibble
@@ -15,18 +24,35 @@ check_flatline <- function(df){
   }
   
   # df must have at least 1 row
+  # this implies df will have at least 1 col as well
   if (is.null(nrow(df))){
     stop("df must be a data.frame of at least 1 row")
   }
   
-  # df must have at least 1 col
-  if (is.null(ncol(df))){
-    stop("df must be a data.frame of at least 1 column")
+  # basic imputation using one of the min, max or mean
+  impute_values <- apply(df[vars],2,
+                         function(x){
+                           if (impute_type=="min"){
+                             min(x,na.rm = TRUE)  
+                             } else if (impute_type=="max"){
+                             max(x,na.rm = TRUE)
+                             } else if (impute_type=="mean"){
+                             mean(x,na.rm = TRUE)
+                             }
+                           }
+                         )
+  
+  for (i in seq_along(vars)){
+    print(df[is.na(df[,i]),i]) <- impute_values[i]
   }
   
-  return_df <- as.data.frame(apply(df,1,
+  if (sum(is.na(df))){
+    warning("there are still missing values in your data")
+  }
+  
+  return_df <- as.data.frame(apply(df[vars],1,
                                    function(x){
-                                     max(table(x,useNA = "always"))
+                                     max(table(x))
                                      }
                                    )
                              )
