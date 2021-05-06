bc245eb2eec63f28352231260bf501117deab2ab
Munsamy <Prenolan.munsamy@kantar.com>
2021-05-06 08:14:46 +0200

    updated documentation of factor_segmentation.R

--- R/factor_segmentation.R
+++ R/factor_segmentation.R
@@ -1,9 +1,20 @@
 #' runs factor analysis with varimax rotation using the psych package
-#' @export
-#' @param df should be a dataframe of numeric variables
+#' @param df should be a data.frame of numeric variables
+#' 
+#' @param vars must be a string of variable names to operate on.
+#' These variables must be numeric
+#' 
+#' @param impute_type must be a string of one of "none","mode","mean","min","max"
 #' @param num_sols should be a numeric vector specifying the minimum and maximum number of factors to extract
 
-factor_segmentation <- function(df,vars,impute_type,num_sols,weight_var=NULL){
+#' @examples
+#' mydf <- data.frame(col1=c(1,2,3),col2=c(1,3,2),col3=c(1,2,1))
+#' factor_segmentation(df = mydf, vars = c("col1","col2","col3"), impute_type = "none",num_sols=c(3,5))
+
+#' @export
+
+
+factor_segmentation <- function(df,vars,impute_type,num_sols,weight_var){
   
   # ensuring df is provided
   if (missing(df)){
