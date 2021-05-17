d83888bddbf348e140e36f09af1fe844388a6bb5
Munsamy <Prenolan.munsamy@kantar.com>
2021-05-17 17:11:36 +0200

    updated tests for check_flatline.R

--- tests/testthat/test_check_flatline.R
+++ tests/testthat/test_check_flatline.R
@@ -1,3 +1,17 @@
 test_that("proportion for 1 variable input is 1 for all rows",{
-  expect_equal(check_flatline(data.frame(x=c(1,2,3))),c(1,1,1))
+  expect_equal(check_flatline(data.frame(c(1,2,3)),1),
+               data.frame(Prop_Flatline=c(1,1,1)))
+})
+
+test_that("proportion for 2 equal variables is 1 for rows",{
+  expect_equal(check_flatline(data.frame(x=c(1,2,3),
+                                         y=c(1,2,3)),c("x","y")),
+               data.frame(Prop_Flatline=c(1,1,1)))
+})
+
+test_that("proportion for 3 variables",{
+  expect_equal(check_flatline(data.frame(x=c(1,2,1),
+                                         y=c(2,2,2),
+                                         z=c(1,2,3)),c("x","y","z")),
+               data.frame(Prop_Flatline=c(2/3,1,1/3)))
 })
