95688c4d700a0dcc7280d442aa39317c9d568ae1
Munsamy <Prenolan.munsamy@kantar.com>
2021-05-27 08:11:40 +0200

    updated segmentation.R to have a new argument. users can now choose which clustering technique to run instead of running all of them with each call of the function

--- R/segmentation.R
+++ R/segmentation.R
@@ -5,49 +5,55 @@
 #' 
 #' @param vars must be a string of variable names to operate on.
 #' These variables must be numeric
-#' @param min_segs should be an integer specifying the minimum number of segments to extract, defaults to 3
-#' @param max_segs should be an integer specifying the maximum number of segments to extract, defaults to 5
-
+#' @param min_segs must be an integer specifying the minimum number of segments to extract, defaults to 3
+#' @param max_segs must be an integer specifying the maximum number of segments to extract, defaults to 5
+#' @param weight must be a character vector of the name of the row weight or NULL
+#' @param method must be a character vector of at least one of 'factor' or 'lca'
 #' @examples
 #' mydf <- data.frame(col1=c(1,2,3),col2=c(1,3,2),col3=c(1,2,1))
-#' segmentation(df = mydf, vars = c("col1","col2","col3"),min_segs = 3, max_segs = 5)
+#' segmentation(df = mydf, vars = c('col1','col2','col3'),min_segs = 3, max_segs = 5,method = c('factor'))
 #' @export
 
-segmentation <- function(df, vars, min_segs = 3, max_segs = 5, weight_var = NULL){
+segmentation <- function(df, vars, min_segs = 3, max_segs = 5, weight_var = NULL, method = c('factor')){
 
   #dist_start <- base::Sys.time()
-  #print(paste0("calculating distance matrix: ",dist_start))
+  #print(paste0('calculating distance matrix: ',dist_start))
 
-  segmentation_out <- vector("list",length = 2)
+  segmentation_out <- vector('list',length = 2)
   num_sols <- seq(min_segs,max_segs)
 
-  #gower_dist <- cluster::daisy(df,metric = "gower")
+  #gower_dist <- cluster::daisy(df,metric = 'gower')
 
   #kmeans_start <- base::Sys.time()
-  #print(paste0("starting kmeans segmentation: ",kmeans_start))
+  #print(paste0('starting kmeans segmentation: ',kmeans_start))
 
   # kmeans
-  #segmentation_out[["kmeans"]] <- kmeans_segmentation(gower_dist,num_sols)
+  #segmentation_out[['kmeans']] <- kmeans_segmentation(gower_dist,num_sols)
 
   #fac_start <- base::Sys.time()
-  #print(paste0("starting factor segmentation: ",fac_start))
+  #print(paste0('starting factor segmentation: ',fac_start))
 
   # factor analysis
-  segmentation_out[["factor_analysis"]] <- factor_segmentation(df,vars,num_sols,weight_var)
+  if (method %in% 'factor'){
+    segmentation_out[['factor_analysis']] <- factor_segmentation(df,vars,num_sols,weight_var)  
+  }
+  
 
   # lca analysis
-  segmentation_out[["lca"]] <- lca_segmentation(df,vars,num_sols)
+  if (method %in% 'lca'){
+    segmentation_out[['lca']] <- lca_segmentation(df,vars,num_sols)  
+  }
   
   #kmediods_start <- base::Sys.time()
-  #print(paste0("starting kmediods segmentation: ",kmediods_start))
+  #print(paste0('starting kmediods segmentation: ',kmediods_start))
 
   # kmediods
-  #segmentation_out[["kmediods"]] <- kmediods_segmentation(gower_dist,num_sols)
+  #segmentation_out[['kmediods']] <- kmediods_segmentation(gower_dist,num_sols)
 
   #timeend <- base::Sys.time()
-  #print(paste0("segmentation complete: ",timeend))
+  #print(paste0('segmentation complete: ',timeend))
 
-  #print(paste0("duration: ",timeend-dist_start))
+  #print(paste0('duration: ',timeend-dist_start))
 
   return(segmentation_out)
 }
