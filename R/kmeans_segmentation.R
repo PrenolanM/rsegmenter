ec2427610ab317f69467e77aeda28f858c78352f
Munsamy <Prenolan.munsamy@kantar.com>
2021-07-28 13:39:56 +0200

    updated kmeans_segmentation function and documentation

--- R/kmeans_segmentation.R
+++ R/kmeans_segmentation.R
@@ -1,26 +1,35 @@
-#' runs kmeans
+#' runs kmeans using the stats package
+#' @param df data.frame of numeric variables
+#' @param vars character vector of variable names
+#' @param num_sols numeric vector specifying the minimum and maximum number of segments to extract
+#' @examples
+#' mydf <- data.frame(col1=c(1,2,3),col2=c(1,3,2),col3=c(1,2,1))
+#' kmeans_segmentation(df = mydf, vars = c("col1","col2","col3"), num_sols = c(2,3))
+#' @export
+#'
+kmeans_segmentation <- function(df,
+                                vars,
+                                num_sols,
+                                iter.max = 10,
+                                nstart = 1,
+                                algorithm = c("Hartigan-Wong", "Lloyd", "Forgy",
+                                              "MacQueen")){
 
-#' @param dist_mat should be a distance matrix
-#' @param num_sols should be a numeric vector specifying the minimum and maximum number of factors to extract
-
-kmeans_segmentation <- function(dist_mat,num_sols){
-
-  # need to test that dist_mat is a distance matrix
-
+  df <- df[,vars,drop=FALSE]
+  
   kmeans_segs <- lapply(num_sols,
                         function(x){
 
-                          set.seed(12345)
+                          set.seed(123456)
 
-                          stats::kmeans(dist_mat,
-                                        centers =x
-                                        #iter.max = 100,
-                                        #nstart = 10
+                          stats::kmeans(df,
+                                        centers = x,
+                                        iter.max = iter.max,
+                                        nstart = nstart,
+                                        algorithm = "Hartigan-Wong"
                                         )
-
                           }
                         )
-
 
   return(kmeans_segs)
 
